097b1f528bbf2d8ed7b281f83287fb66f384d79d
Panaite Cristian <panaite.cristian99@gmail.com>
2022-12-10 18:39:57 +0200

    why

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import Image from "next/image";
+import fun from "../public/fun.png";
+import general from "../public/general.png";
+import moderation from "../public/moderation.png";
+import owner from "../public/owner.png";
+import stats from "../public/stats.png";
 
 function About() {
   return (
@@ -6,7 +12,57 @@ function About() {
       <div className="about-heading">
         <h1>Why Ortsero?</h1>
         <hr></hr>
-        <h1>test</h1>
+      </div>
+      <div className="why">
+        <div className="fun">
+          <div className="nush">
+            <h2>Fun</h2>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+          </div>
+          <Image src={fun} alt="fun" />
+        </div>
+        <hr></hr>
+        <div className="fun">
+          <div className="nush">
+            <h2>Fun</h2>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+          </div>
+          <Image src={fun} alt="fudddadadadn" />
+        </div>
+        <hr></hr>
+        <div className="fun">
+          <div className="nush">
+            <h2>Fun</h2>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+          </div>
+          <Image src={fun} alt="dd" />
+        </div>
+        <hr></hr>
+        <div className="fun">
+          <div className="nush">
+            <h2>Fun</h2>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+          </div>
+          <Image src={owner} alt="fuasdn" />
+        </div>
+        <hr></hr>
+        <div className="fun">
+          <div className="nush">
+            <h2>Fun</h2>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+            <li>asdasdsad</li>
+          </div>
+          <Image src={stats} alt="fuddadn" />
+        </div>
       </div>
     </div>
   );
